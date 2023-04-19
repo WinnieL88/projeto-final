acfd6daae52eb7b2e084ba81d35a83b2ff673673
WinnieL88 <82378456+WinnieL88@users.noreply.github.com>
2023-04-19 17:56:47 -0300

    Initial commit

---
+++ src/app/produtos/produtos.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProdutosComponent } from './produtos.component';
+
+
+
+@NgModule({
+  declarations: [
+    ProdutosComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ProdutosModule { }
